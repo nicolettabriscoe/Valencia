06d9898c106e4548cfd473bdc188ac94c308b2ca
EC2 Default User <ec2-user@ip-172-31-44-75.us-east-2.compute.internal>
2018-08-03 17:38:54 +0000

    first trial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,44 @@ require './app/models/sample_model'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    set :views, 'app/views/home'
   end
 
   get '/' do
     return erb :index
+  end
+  
+  get '/quiz' do
+    return erb :quiz
+  end  
+  
+  get '/bullying' do
+    return erb :bullying
+  end
+  
+  get '/sexism' do
+    return erb :sexism
+  end 
+  
+  get '/racism' do 
+    return erb :racism
+  end 
+  
+  get '/lgbtq' do 
+    return erb :lgbtq
+  end  
+  
+  get '/environmental' do
+    return erb :environmental
+  end
+
+  post "/quiz" do
+    @ptsarray = params.values
+    @sexism_response= params[:sexism]
+    @lgbtq_response = params[:lgbtq]
+  
+    quiz = Quiz.new
+    @message = quiz.play_quiz(@ptsarray)
+    return erb :results
   end
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,74 @@
-class SampleModel
-  attr_accessor :name, :age
-end
+class Quiz
+    # def questions(score)
+    #     if score >="1" && score <="10"
+    #         return score
+    #     # else
+    #     #   passed_score = false
+    #     #   while passed_score == false
+           
+    #     #   score = gets.strip
+    #     #      if score >="1" && score <="10"
+    #     #           return score
+    #     #      end
+    #     #   end
+    #     end
+    # end
+    
+    
+    # sexism = questions(sexism)
+    
+    # lgbtq = questions(lgbtq)
+    
+    # bully = questions(bully)
+    
+    # environment = questions(environment)
+    
+    # racism = questions(racism)
+    
+    def initialize
+        @highest_score_index = []
+        @page_links = []
+        @results = ["<a href=\"/sexism\">Sexism</a>", "<a href=\"/lgbtq\">LGBTQ</a>", "<a href=\"/bullying\">Bullying</a>", "<a href=\"/environmental\">Environment</a>", "<a href=\"/racism\">Racism</a>"]
+
+     end
+     
+   
+    def high_score(ptsarray)
+        index = 0
+        highest_score = 0
+       
+        ptsarray.each do |score|
+            if score.to_i >= highest_score.to_i
+                highest_score = score
+                @highest_score_index << index
+            end
+            index += 1
+        end
+    end
+    
+   
+    def links
+        @highest_score_index.each do |high_index|
+            @page_links << @results[high_index]
+        end
+        temp = ""
+        curr = 0
+        @page_links.each do |bob|
+            if curr != @page_links.size-1
+               temp += bob + ", " 
+           else
+               temp += bob
+            end
+            curr+=1
+        end
+        return temp
+    end
+    
+    def play_quiz(ptsarray)
+        high_score(ptsarray)
+        links
+    end  
+        
+        
+    
+end 
